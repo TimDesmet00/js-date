60ffcd6680faf26dbfb6d055e75259d22d0b9c39
tim <timdesmet00@gmail.com>
2023-10-03 07:40:29 +0200

    petite correction sur les jours

--- bonus/JS/script.js
+++ bonus/JS/script.js
@@ -4,13 +4,34 @@ const datesMonth = document.querySelector(".dates-month");
 const years = document.querySelector(".years");
 const hours = document.querySelector(".hours");
 
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthOfYears = [
+  "jan",
+  "feb",
+  "mar",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
+
 let bruxelleDate = new Date();
 
 console.log(bruxelleDate);
 
-days.textContent = `${bruxelleDate.getDay()}`;
+days.textContent = daysOfWeek[bruxelleDate.getDay()];
 datesDay.textContent = `${bruxelleDate.getDate()}`;
-datesMonth.textContent = `${bruxelleDate.getMonth()}`;
+datesMonth.textContent = monthOfYears[bruxelleDate.getMonth() - 1];
 years.textContent = `${bruxelleDate.getFullYear()}`;
+hours.textContent = `${bruxelleDate.getHours()}:${bruxelleDate.getMinutes()}:${bruxelleDate.getSeconds()}`;
 
-hours.textContent= `${bruxelleDate.getHours()}:${bruxelleDate.getMinutes()}:${bruxelleDate.getSeconds()}`;
+let updateTime = () => {
+  bruxelleDate = new Date();
+  hours.textContent = `${bruxelleDate.getHours()}:${bruxelleDate.getMinutes()}:${bruxelleDate.getSeconds()}`;
+};
+
+setInterval(updateTime, 1000);
